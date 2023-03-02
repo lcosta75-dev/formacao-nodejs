690818f0b30b34fecd109e31612f1a4c5526ee39
Lais Costa <lcosta.developer@gmail.com>
2023-03-02 16:29:48 -0300

    Subindo repositório

---
+++ inicio/app.js
@@ -0,0 +1,12 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com"
+
+console.log("Hello Word")
+console.log("Meu nome é Lais")
+console.log("E eu estou aprendendo Nodejs com o Guia do Programador")
+
+if (mostrarSite) {
+    console.log(site)
+}
+
+
